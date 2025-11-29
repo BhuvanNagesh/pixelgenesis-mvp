f7e12c67b35c8916e7a5feac1d12546e6b6947d0
BhuvanNagesh <109874160+BhuvanNagesh@users.noreply.github.com>
2025-11-30 01:32:01 +0530

    Updated app.py

--- app.py
+++ app.py
@@ -566,4 +566,4 @@ def sign_vc():
 # ============================================================
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
